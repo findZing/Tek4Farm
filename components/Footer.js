24b2ebb06e3b5e4a3a1097bdea70a308accc29e4
Manh <manhnguyenhuu1001@gmail.com>
2023-04-02 22:43:02 +0700

    Change

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-[200px] border-t border-black flex flex-row py-[20px] justify-center'>
+    <div className='w-full h-[200px] border-t border-black flex flex-row py-[20px] justify-center gap-[30px]'>
         <p>Copyright @ by Tek4Farm</p>
+
+        <div className='flex flex-col items-center gap-[10px]'>
+          <span>Liên hệ với chúng tôi tại:</span>
+          <span>InfoFarmer@InfoFarmer.com</span>
+        </div>
     </div>
   )
 }
