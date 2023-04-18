f8e4f4b6bb979770a9c9ba71bd721450e793ca4a
Nguyễn Hữu Mạnh <manhnguyenhuu1001@gmail.com>
2023-04-18 21:52:21 +0700

    Change

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href='../public/assets/images/favicon.ico' />
+
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,16 @@ import Image from "next/image";
 import React from "react";
 
 import banner from "../public/assets/images/banner.jpg";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Trang chủ</title>
+        <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+
+      </Head>
       <div className="max-w-content w-full mx-auto flex flex-row justify-between gap-[20px] pt-[20px]">
         <Image src={banner} className="w-[70%] h-[300px] object-cover rounded-[12px]" />
         <Image src={banner} className="w-[30%] h-[300px] object-cover rounded-[12px]" />
